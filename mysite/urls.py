9cca776ac808fb789ab826ba121771cdb41ef755
caraplana1 <cristianlandaez@gmail.com>
2025-07-17 18:32:22 -0400

    error arreglado

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from chati.views import hello_world, index
+from chati.views import index
 import chati.urls
 
 urlpatterns = [
