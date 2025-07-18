12a969456cc769c962e80b695346a939f46078fb
caraplana1 <cristianlandaez@gmail.com>
2025-07-17 23:51:43 -0400

    rerouting

--- chati/urls.py
+++ chati/urls.py
@@ -3,21 +3,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('inicio', views.index, name='index'),
     path("login", views.log_in),
     path("login/password_recover", views.recover_pass),
     path("signin", views.sign_in),
     path("login/password_recover/change_pass", views.change_pass),
     path("chati/edit_profile/change_pass", views.change_pass),
-    path("chatrequest", views.chat_request),
 
-    path("chati", views.main),
-    path("chati/chats", views.chats),
-    path("chati/chats/requests", views.chat_request),
-    path("chati/chat-user", views.chatting),
-    path("chati/friends", views.friends),
-    path("chati/profile", views.profile),
-    path("chati/profile/edit_user", views.edit_user),
-    path("chati/notifications", views.notification),
-    path("chati/post", views.post),
+    path("feed", views.main),
+    path("feed/chats", views.chats),
+    path("feed/chats/request", views.chat_request),
+    path("feed/chat-user", views.chatting),
+    path("feed/friends", views.friends),
+    path("feed/profile", views.profile),
+    path("feed/profile/edit_user", views.edit_user),
+    path("feed/notifications", views.notification),
+    path("feed/post", views.post),
 ]
