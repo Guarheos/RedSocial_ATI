a6451f7b69699f412804e5d3074617057aabcc10
Ichigoz <anagabi0703@gmail.com>
2025-07-21 20:24:12 -0400

    componentes bd

--- chati/admin.py
+++ chati/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Profile, Post, Comment
+from .models import Profile, Post, Comment, Friendship, Chat, Message, Settings
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(Friendship)
+admin.site.register(Chat)     
+admin.site.register(Message) 
+admin.site.register(Settings)
