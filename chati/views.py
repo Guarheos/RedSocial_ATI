2751eda831bd81fb9c1add25f6e50c8fba165408
caraplana1 <cristianlandaez@gmail.com>
2025-07-22 20:14:39 -0400

    metodos para la obtencion de datos dinamicos

--- chati/views.py
+++ chati/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Profile, Post, Comment, Friendship, Chat, Message
+
+def main(request):
+    posts = Post.objects.all().order_by('-post_date')
+    return render(request, "chati/MainPage.html", {'posts': posts})
+
+def profile(request):
+    user_profile = get_object_or_404(Profile, user=request.user)
+    return render(request, "chati/ProfileUser.html", {'profile': user_profile})
+
+def friends(request):
+    friendships = Friendship.objects.filter(
+        Q(requester=request.user) | Q(receiver=request.user),
+        status='accepted'
+    )
+    return render(request, "chati/Friends.html", {'friendships': friendships})
+
+def post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    comments = Comment.objects.filter(post=post).order_by('comment_date')
+    return render(request, "chati/Publication.html", {'post': post, 'comments': comments})
+
+def chatting(request, chat_id):
+    chat = get_object_or_404(Chat, id=chat_id)
+    messages = Message.objects.filter(chat=chat).order_by('sent_date')
+    return render(request, "chati/ChattingUser.html", {'chat': chat, 'messages': messages})
+
 
 
 def index(request):
